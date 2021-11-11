d3420110f9c34757231ef2261cf5ba7d90332aa2
nirupan <nirupanjoej@gmail.com>
2021-11-11 09:58:57 +0530

    Refactored OrbitControls.

--- src/components/modelCanvas.js
+++ src/components/modelCanvas.js
@@ -1,13 +1,29 @@
-import { React, Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Environment, OrbitControls } from '@react-three/drei';
+import { React, Suspense, useEffect } from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Model from './model';
 
+const CameraController = () => {
+	const { camera, gl } = useThree();
+
+	useEffect(() => {
+		const controls = new OrbitControls(camera, gl.domElement);
+
+		controls.minDistance = 3;
+		controls.maxDistance = 20;
+		return () => {
+			controls.dispose();
+		};
+	},
+	[camera, gl]);
+	return null;
+};
 const ModelCanvas = () =>
 	<Canvas>
+		<CameraController/>
 		<Suspense fallback={ null }>
 			<Model/>
-			<OrbitControls/>
 			<Environment preset="sunset" background={ true }/>
 		</Suspense>
 	</Canvas>;
