b4692607fe7f39d0e6c77132ab3f3bc75848ad23
NirupanJoe <nirupanjoej@gmail.com>
2021-11-26 14:29:33 +0000

    Bug Fixed.

--- src/components/characters/characterCanvas.js
+++ src/components/characters/characterCanvas.js
@@ -1,7 +1,7 @@
 import { React, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Character from './character.js';
-import CameraController from './cameraController.js';
+import CameraController from '../cameraController.js';
 
 const CharacterCanvas = () =>
 	<Canvas>
