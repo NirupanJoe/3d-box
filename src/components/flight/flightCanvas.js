0a0e1cae65cd1ce417597aefcef7d372aa8e69fb
NirupanJoe <nirupanjoej@gmail.com>
2021-12-02 19:22:08 +0530

    Dusted

--- src/components/flight/flightCanvas.js
+++ src/components/flight/flightCanvas.js
@@ -1,14 +1,12 @@
 import { Canvas } from '@react-three/fiber';
-import { React, Suspense } from 'react';
+import { React } from 'react';
 import Flight from './flight';
 
 const FlightCanvas = () =>
 	<Canvas>
 		<ambientLight intensity={ 0.6 }/>
 		<directionalLight intensity={ 0.5 }/>
-		<Suspense fallback={ null }>
-			<Flight/>
-		</Suspense>
+		<Flight/>
 	</Canvas>;
 
 export default FlightCanvas;
